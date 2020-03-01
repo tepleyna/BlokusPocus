9be0bda72ca2257ee14d05c5230b557bbf08a34b
Tasha Tepley <tepleyna@gmail.com>
2020-03-01 10:56:23 -0600

    pretty print for legible debugging

--- app/Piece.py
+++ app/Piece.py
@@ -1,4 +1,12 @@
 class Piece:
-    
+
     def __init__(self, shape):
         self.shape = shape
+
+    def pretty_print(self):
+        ## TODO: make the print pretty
+        for line in self.shape:
+            out = ""
+            for c in line:
+                out += "x" if c == "x" else "_"
+            print(out)

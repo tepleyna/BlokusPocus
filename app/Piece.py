6dbe15821b396c9ebc0dcc8b6a3094505295698a
Tasha Tepley <tepleyna@gmail.com>
2020-03-02 15:10:28 -0600

    shape has id, shape getter, and shape returns a deepcopy so it is not mutated accidentally

--- app/Piece.py
+++ app/Piece.py
@@ -1,11 +1,73 @@
+from copy import deepcopy
+
 class Piece:
 
-    def __init__(self, shape):
-        self.shape = shape
+    def __init__(self, shape, id):
+        self._shape = shape
+        self.id = id
 
-    def pretty_print(self):
-        for line in self.shape:
-            out = ""
-            for c in line:
-                out += "x" if c == "x" else "_"
-            print(out)
+    def getShape(self):
+        return deepcopy(self._shape)
+
+    def getId(self):
+        return self.id
+
+    def prettyPrint(self):
+        print(self.shapeAsText())
+
+    def shapeAsText(self):
+        return ("\n").join(
+            ( ("").join(line) ) for line in self._shape)
+
+
+def allShapes():
+    return ([
+    [ ['x'] ] ,     # 1
+    [ ['x', 'x']],
+
+    [ ['x','x','x']], # 3
+    [ ['x','x'],
+        ['x','o']],
+
+    [ ['x','x','x','x']], # 5
+    [ ['x','x','x'],
+        ['x','o','o']],
+    [ ['o','x','o'],
+        ['x','x','x']],
+    [ ['x','x'],
+        ['x','x']],
+    [ ['o','x'],
+        ['x','x'],
+        ['x','o']],
+
+    [ ['x','x','x','x','x']], #10
+    [ ['x','x','x'],
+        ['x','o','o'],
+        ['x','o','o']],
+    [ ['x','x','x','x'],
+        ['o','o','x','o']],
+    [ ['x','x','x','x'],
+        ['o','o','o','x']], # 13
+    [ ['o','x','x'],
+        ['x','x','o'],
+        ['x','o','o']],
+    [ ['o','x','o'],
+        ['x','x','x'],
+        ['o','x','o']],
+    [ ['x','x','x','o'],
+        ['o','o','x','x']],
+    [ ['o','x','x'],
+        ['o','x','o'],
+        ['x','x','o']], #17
+    [ ['o','x','x'],
+        ['x','x','o'],
+        ['o','x','o']],
+    [ ['x','x','x'],
+        ['o','x','o'],
+        ['o','x','o']],
+    [ ['o','x','x'],
+        ['o','o','x'],
+        ['o','x','x']],
+    [ ['x','x','x'],
+        ['x','x','o']] #21
+    ])
--- unit_tests/piece.ut.py
+++ unit_tests/piece.ut.py
@@ -1,15 +1,56 @@
-import unittest, sys
+import unittest, sys, copy
 
 sys.path.append("../")
 sys.path.append("../..")
-from BlokusPocus.app.piece import Piece
+from BlokusPocus.app.piece import Piece, allShapes
 
-class TestBasicFunction(unittest.TestCase):
-    # def setUp(self):
-    #     self.func = ClassGoesHere()
+class TestPiece(unittest.TestCase):
 
-    def test(self):
-        self.assertTrue(True)
+    # TODO: shape as text
+    # TODO: rotatec rotatecc flipxy shape  (mutate and getter)
+
+    def testSetUp(tp):
+        shapes = allShapes()
+        pieces = []
+        for s in shapes:
+            pieces.append(Piece(s, 0))
+
+        tp.assertFalse(len(pieces) == 0)
+        tp.assertTrue(len(pieces) == len(shapes))
+        for p in pieces:
+            with tp.subTest():
+                tp.assertIsInstance(p, Piece)
+
+    def testGetIdEqual(tp):
+        # after this, we can use getId() in subtests for identity
+        id = 12
+        p = Piece( [["x"]] , id)
+        tp.assertEqual(p.getId(), id)
+        tp.assertNotEqual(p.getId(), id+7)
+
+    def testGetShapeEqual(tp):
+        shapes = allShapes()
+
+        i = 0
+        for shape in shapes:
+            i += 1
+            with tp.subTest(j = i):
+                    p = Piece(shape, i)
+                    # not using assertListEqual()because order matters
+                    piece_shape = p.getShape()
+                    tp.assertEqual(piece_shape, shape)
+                    tp.assertNotEqual(piece_shape, [['o']])
+
+    def testShapesNotMutable(tp):
+        original_shape = [ ['x','x','o'] ]
+        piece = Piece(copy.deepcopy(original_shape), 3) #splicing ensures original is safe
+
+        mutated_copy = piece.getShape()
+        mutated_copy[0][1] = 'M'
+
+        piece_shape = piece.getShape()
+        tp.assertEqual(piece_shape, original_shape)
+        tp.assertNotEqual(piece_shape, mutated_copy)
 
 if __name__ == '__main__':
     unittest.main()

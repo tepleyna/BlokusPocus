ad4ddc9c42ff999991ca2e6c17609c07dcf59df1
Tasha Tepley <tepleyna@gmail.com>
2020-03-04 09:01:28 -0600

    player getter methods

--- app/Player.py
+++ app/Player.py
@@ -1,4 +1,15 @@
 class Player:
 
-    def __init__(self):
-        pass
+    def __init__(self, display_name, id, color):
+        self._display_name = display_name
+        self._id = id
+        self._color = color
+
+    def getDisplayName(self):
+        return self._display_name
+
+    def getId(self):
+        return self._id
+
+    def getColor(self):
+        return self._color
--- unit_tests/player.ut.py
+++ unit_tests/player.ut.py
@@ -5,4 +5,15 @@ sys.path.append("../..")
 from BlokusPocus.app.player import Player
 
 class TestPlayer(unittest.TestCase):
-    pass
+
+    def setUp(test):
+        test.name = "Tracey"
+        test.id = 1
+        test.color = "red"
+        test.player = Player(test.name, test.id, test.color)
+
+    def testGetDisplayName(test):
+        assertEqual(test.player.getDisplayName(), test.name)
+
+    def testBlankDisplayName(test):
+        assertEqual(test.player.getDisplayName(), test.name)
